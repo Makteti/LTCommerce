b977a02f28e64102399c1c40b2ac1aaab6cf5f8c
Mwerynski <93839298+Mwerynski@users.noreply.github.com>
2024-01-16 14:03:05 +0100

    Interface
    Added a GUI and a better functionality.

--- app/__init__.py
+++ app/__init__.py
@@ -20,17 +20,17 @@ def add_item():
     Item.create(name=name)
     return redirect('/')
 
-@app.route('/delete/<item_id:int>')
+@app.route('/delete/<item_id:int>', method='POST')
 def delete_item(item_id):
     item = Item.get(Item.id == item_id)
     item.delete_instance()
-    return redirect(my_url_for('index'))
+    return redirect('/')
 
-@app.route('/change_status/<item_id:int>')
+@app.route('/change_status/<item_id:int>', method='POST')
 def change_status(item_id):
     item = Item.get(Item.id == item_id)
     item.status = 'Unavailable' if item.status == 'Available' else 'Available'
     item.save()
-    return redirect(my_url_for('index'))
+    return redirect('/')
 
 init_app(app)
